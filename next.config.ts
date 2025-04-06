b1d44eec70d14a79cf2e91a93f6a666a49833011
José Hernanes <68444005+devjoseh@users.noreply.github.com>
2025-04-06 00:29:45 -0300

    feat: increased image size limit and new route

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 module.exports = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "skillicons.dev",
-      },
+      { protocol: "https", hostname: "skillicons.dev" },
+      { protocol: "https", hostname: `${process.env.NEXT_PUBLIC_SUPABASE_ID}.supabase.co` },
     ],
     dangerouslyAllowSVG: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb',
+    },
   },
 };
